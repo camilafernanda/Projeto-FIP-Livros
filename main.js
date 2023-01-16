5b241ceea3b1a6ec5a1bfb0ac54c3c971a19fcac
Camila Fernanda <millanandanet14@gmail.com>
2023-01-16 14:11:06 -0300

    Atualizando projeto p/ Github Pages

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let totalPaginas = [];
 let vetorPaginasLidas = [];
 let vetorPaginasNaoLidas = [];
 
-function extraiValores() {
+function extraiValoresAoCarregarAPágina() {
     tituloLivro = document.querySelector('#titulo');
     capaLivro = document.querySelector('#capa_livro');
     statusLeitura = document.querySelector('#status');
@@ -21,53 +21,109 @@ function adicionaLivro() {
     let cellLivroLido = linha.insertCell();
     let cellLivroNaoLido = linha.insertCell(1);
 
-    // Aqui eu tentei fazer a imagem com o Image e o botão de delete com createElement tb, mas aí eu deveria usar 3 appendChild em uma célula da tabela e, pelo o que pesquisei, não tem como.
-    // let novoLivro = new Image(100, 200);
-    // novoLivro.src = capaLivro.value;
-    let novoLivro = "<img src=" + capaLivro.value + ">" + "<br>" + tituloLivro.value;
- 
+    let novoLivro = new Image(100, 200);
+    novoLivro.src = capaLivro.value;
+
+    // let novoLivro = "<img src=" + capaLivro.value + ">" + "<br>" + tituloLivro.value;
+    elementoDiv = criaDiv(tituloLivro);
+    botaoDelete = criaBotaoDelete(cellLivroLido, cellLivroNaoLido, statusLeitura);
+    botaoLido = criaBotaoLido(novoLivro, cellLivroLido, cellLivroNaoLido, elementoDiv, botaoDelete);
+    
     if (statusLeitura.value == 'sim') {
-        cellLivroLido.innerHTML = novoLivro +
-        "<br><button onclick='deletaLivro(this)'>Delete</button>";
+        cellLivroLido.appendChild(novoLivro);
+        cellLivroLido.append(elementoDiv);
+        cellLivroLido.appendChild(botaoDelete);
+        //cellLivroLido.innerHTML = tituloLivro.value + botaoDelete;
 
         vetorPaginasLidas.push(Number(qtdPaginas.value));
     } else {
-        let botaoLido = document.createElement("button");
-        botaoLido.id = "lido";
-        botaoLido.innerHTML = "Lido";
-        cellLivroNaoLido.innerHTML = novoLivro +
-        "<br> <button onclick='deletaLivro(this)'>Delete</button>";
-        cellLivroNaoLido.appendChild(botaoLido); 
-
-        botaoLido.addEventListener('click', () => {
-        marcarComoLido(novoLivro, cellLivroLido)
-        cellLivroNaoLido.remove();
-        })
+        cellLivroNaoLido.appendChild(novoLivro);
+        cellLivroNaoLido.append(elementoDiv);
+        cellLivroNaoLido.appendChild(botaoDelete);
+        cellLivroNaoLido.appendChild(botaoLido);
+        //cellLivroNaoLido.innerHTML = tituloLivro.value + botaoDelete;
 
         vetorPaginasNaoLidas.push(Number(qtdPaginas.value));
     } 
 
-    contaPaginas(vetorPaginasLidas, vetorPaginasNaoLidas);
+    contaPaginas();
 
 }
 
-function deletaLivro(livroAdeletar) {
+function criaDiv(tituloLivro) {
+    let elementoDiv = document.createElement("div");
+    elementoDiv.id = "div_titulo";
+    elementoDiv.innerHTML = tituloLivro.value; 
+    return elementoDiv;
+}
+
+function criaBotaoDelete(cellLivroLido, cellLivroNaoLido, statusLeitura) {
+    // let botaoDelete = "<br><button onclick='deletaLivroLido(this)'>Delete</button>";
+    // return botaoDelete;
+
+    let botaoDelete = document.createElement("button");
+    botaoDelete.id = "delete";
+    botaoDelete.innerHTML = "Delete";
+    botaoDelete.addEventListener('click', function() {
+        if (statusLeitura.value == 'sim') {
+            cellLivroLido.remove();
+            deletaLivroLido(this);
+        } else {
+            cellLivroNaoLido.remove();
+            deletaLivroNaoLido(this);
+        }
+    });
+    return botaoDelete;
+}
+
+function criaBotaoLido(novoLivro, cellLivroLido, cellLivroNaoLido, elementoDiv, botaoDelete) {
+    let botaoLido = document.createElement("button");
+    botaoLido.id = "lido";
+    botaoLido.innerHTML = "Lido";
+    botaoLido.addEventListener('click', function() {
+    marcarComoLido(novoLivro, cellLivroLido, elementoDiv, botaoDelete);
+    cellLivroNaoLido.remove();
+    });
+    return botaoLido;
+}
+
+function deletaLivroLido(livroAdeletar) {
     livroAdeletar.parentElement.remove();
-    // Parei aqui: fazendo a contagem de páginas diminuir assim que algum livro for deletado
-    console.log(totalPaginas);
+    //vetorPaginasLidas.pop;
+    paginas = contaPaginas();
+    paginasLidas = paginas[0];
+    for(let i=0; i<vetorPaginasLidas.length; i++) {
+        paginasLidas -= vetorPaginasLidas[i];
+    }
+    console.log(paginasLidas);
+    console.log(vetorPaginasLidas);
 }
 
-function marcarComoLido(novoLivro, cellLivroLido) {
-    cellLivroLido.innerHTML = novoLivro +
-        "<br><button style='visual.css' onclick='deletaLivro(this)'>Delete</button>";
+function deletaLivroNaoLido(livroAdeletar) {
+    livroAdeletar.parentElement.remove();
+    //vetorPaginasNaoLidas.pop;
+    //contaPaginas();
+    for(let i=0; i<vetorPaginasNaoLidas.length; i++) {
+        paginasNaoLidas -= vetorPaginasNaoLidas[i];
+    }
+    //console.log(paginasNaoLidas);
+    //console.log(vetorPaginasNaoLidas);
 }
 
-function contaPaginas(vetorPaginasLidas, vetorPaginasNaoLidas) {
+function marcarComoLido(novoLivro, cellLivroLido, elementoDiv, botaoDelete) {
+    cellLivroLido.appendChild(novoLivro);
+    cellLivroLido.append(elementoDiv);
+    cellLivroLido.appendChild(botaoDelete);
+}
+
+function contaPaginas() {
     
     let paginasLidas = 0;
     let paginasNaoLidas = 0;
+    let valorTotalPaginas = 0;
+    totalPaginas.push(Number(qtdPaginas.value));
     
-
+    
     for(let i=0; i<vetorPaginasLidas.length; i++) {
         paginasLidas += vetorPaginasLidas[i];
     }
@@ -76,17 +132,14 @@ function contaPaginas(vetorPaginasLidas, vetorPaginasNaoLidas) {
         paginasNaoLidas += vetorPaginasNaoLidas[i];
     }
 
-    totalPaginas.push(Number(qtdPaginas.value));
-    let valorTotalPaginas = 0;
-    
-    for(let i=0; i<totalPaginas.length; i++) {
-        valorTotalPaginas += totalPaginas[i];
-    } 
+    valorTotalPaginas =  paginasNaoLidas + paginasLidas;
 
     let percentConclusao = 0;
     percentConclusao = (paginasLidas / valorTotalPaginas)*100;
     
     montarTabelaResumo(paginasLidas, paginasNaoLidas, percentConclusao);
+
+    return [paginasLidas, paginasNaoLidas];
 }
 
 function montarTabelaResumo(paginasLidas, paginasNaoLidas, percentConclusao) {
@@ -97,5 +150,5 @@ function montarTabelaResumo(paginasLidas, paginasNaoLidas, percentConclusao) {
     elementoPaginasNaoLidas.textContent = paginasNaoLidas + " páginas";
 
     let elementoPercentConclusao = document.querySelector('#porcentagem_conclusao');
-    elementoPercentConclusao.textContent = percentConclusao + '%';
+    elementoPercentConclusao.textContent = parseFloat(percentConclusao.toFixed(2)) + '%';
 }
